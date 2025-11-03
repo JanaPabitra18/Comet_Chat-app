e731b083faf50f727d906e8a682673edf932cd97
JanaPabitra18 <jpabitra8535@gmail.com>
2025-11-04 03:38:13 +0530

    Fix backend modify

--- backend/index.js
+++ backend/index.js
@@ -39,11 +39,11 @@ app.use("/api/v1/friends", friendRoute);
 const __dirname1 = path.resolve();
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname1, "/frontend/dist")));
-  app.get("/.*/", (req, res) =>
+  app.use((req, res) =>
     res.sendFile(path.resolve(__dirname1, "frontend", "dist", "index.html"))
   );
 } else {
-  app.get("/", (req, res) => {
+  app.use("/", (req, res) => {
     res.send("API is running...");
   });
 }
