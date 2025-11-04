4fbd24eafccccb0e7ba2802f46642183fe03e7af
JanaPabitra18 <jpabitra8535@gmail.com>
2025-11-04 21:16:47 +0530

    Add root package.json for Render deployment

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,7 +5,7 @@ import tailwindcss from '@tailwindcss/postcss';
 // https://vite.dev/config/
 export default defineConfig({
 
-  plugins: [  react(),tailwindcss()
+  plugins: [  react(),tailwindcss() 
 
   ],
 })
